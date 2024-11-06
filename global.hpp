4809b9da10f67af0c5dc7069001eb8f8d32c4298
SR_team <me@sr.team>
2024-11-07 04:54:52 +0500

    Fix group id visibility

--- global.hpp
+++ global.hpp
@@ -11,13 +11,16 @@ inline CFunctionHook* make_hook(HANDLE plugin_handle, void* hook_address, const 
     dladdr(hook_address, &info);
     assert(info.dli_sname != nullptr && "Destination symbol is hidden");
 
+    std::vector<SFunctionMatch> FNS;
     const auto hookName        = std::string_view{info.dli_sname};
-    const auto functionNamePos = hookName.find(function_name);
-    assert(functionNamePos != std::string_view::npos && "name of destination symbol is not match the `function_name`");
+    const auto                  functionNamePos = hookName.find(function_name);
 
-    const auto     functionNmeWithArgs = hookName.substr(functionNamePos);
+    if (functionNamePos != std::string_view::npos) {
+        const auto functionNmeWithArgs = hookName.substr(functionNamePos);
+        FNS                            = HyprlandAPI::findFunctionsByName(plugin_handle, std::string(functionNmeWithArgs));
+    } else
+        FNS = HyprlandAPI::findFunctionsByName(plugin_handle, function_name);
 
-    auto           FNS  = HyprlandAPI::findFunctionsByName(plugin_handle, std::string(functionNmeWithArgs));
     CFunctionHook* hook = nullptr;
     for (auto& fn : FNS) {
         if (!fn.demangled.contains(namespace_name))
@@ -29,4 +32,44 @@ inline CFunctionHook* make_hook(HANDLE plugin_handle, void* hook_address, const 
 
     return hook;
 }
-#define MAKE_HOOK(plugin_handle, destination, namespace_name) make_hook(plugin_handle, (void*)&hook::destination, #destination, namespace_name)
+
+template <typename R, class C, typename... Args>
+inline void* method_cast(R (C::*fn)(Args...)) {
+    struct f {
+        decltype(fn) fn_ptr;
+    } _{fn};
+    return *reinterpret_cast<void**>(&_);
+}
+template <typename To>
+inline To method_cast(void* ptr) {
+    return reinterpret_cast<To>(ptr);
+}
+
+inline void enable_hook(CFunctionHook* hook, int id = 0) {
+    if (hook == nullptr)
+        throw std::runtime_error("[group-in-title] Hook method id:" + std::to_string(id) + " is not found");
+
+    if (!hook->hook())
+        throw std::runtime_error("[group-in-title] Hook id:" + std::to_string(id) + " installation failed");
+}
+inline void disable_hook(CFunctionHook* hook) {
+    if (hook != nullptr)
+        hook->unhook();
+}
+
+template <class... Hooks>
+    requires(std::same_as<Hooks, CFunctionHook*> && ...)
+inline void enable_hooks(Hooks... hooks) try {
+    int i = 0;
+    (enable_hook(hooks, i++), ...);
+} catch (std::exception& e) {
+    (disable_hook(hooks), ...);
+    std::rethrow_exception(std::current_exception());
+}
+template <class... Hooks>
+    requires(std::same_as<Hooks, CFunctionHook*> && ...)
+inline void disable_hooks(Hooks... hooks) try {
+    (disable_hook(hooks), ...);
+} catch (std::exception& e) {
+    // Silent fail
+}
--- main.cpp
+++ main.cpp
@@ -8,55 +8,66 @@
 #include "global.hpp"
 
 // Methods
-static CFunctionHook* g_pFetchWindowHook = nullptr;
-
 namespace hook {
-    EXPORT std::string fetchTitle(CWindow* This) {
-        const auto orig  = (*(decltype(&fetchTitle))g_pFetchWindowHook->m_pOriginal);
-        auto       title = orig(This);
+    class CWindow final : public ::CWindow {
+      public:
+        inline static CFunctionHook* fetchTitleHook          = nullptr;
+        inline static CFunctionHook* insertWindowToGroupHook = nullptr;
 
-        if (!This->m_sGroupData.pNextWindow)
-            return title;
+        EXPORT std::string fetchTitle() {
+            const auto orig  = method_cast<std::string (*)(::CWindow*)>(fetchTitleHook->m_pOriginal);
+            auto       title = orig(this); //(this->*orig)();
 
-        PHLWINDOW head = This->getGroupHead();
-        PHLWINDOW curr = head;
+            if (m_sGroupData.pNextWindow.expired())
+                return title;
 
-        size_t    groupId     = 0;
-        size_t    groupsCount = 0;
-        while (true) {
-            curr = curr->m_sGroupData.pNextWindow.lock();
-            if (curr == head)
-                break;
+            PHLWINDOW head = getGroupHead();
+            PHLWINDOW curr = head;
 
-            groupsCount++;
-            if (curr.get() == This)
-                groupId = groupsCount;
+            size_t    groupId     = 1;
+            size_t    groupsCount = 1;
+            while (true) {
+                curr = curr->m_sGroupData.pNextWindow.lock();
+                if (curr == head)
+                    break;
+
+                groupsCount++;
+                if (curr.get() == this)
+                    groupId = groupsCount;
+            }
+
+            return std::format("[{}/{}] {}", groupId, groupsCount, title);
         }
+        EXPORT void insertWindowToGroup(PHLWINDOW pWindow) {
+            const auto orig = method_cast<void (*)(::CWindow*, PHLWINDOW)>(insertWindowToGroupHook->m_pOriginal);
 
-        return std::format("[{}/{}] {}", groupId + 1, groupsCount, title);
-    }
+            orig(this, pWindow);
+
+            PHLWINDOW curr = pWindow;
+            while (true) {
+                curr = curr->m_sGroupData.pNextWindow.lock();
+                if (curr == pWindow)
+                    break;
+
+                curr->onUpdateMeta();
+            }
+        }
+    };
+    static_assert(sizeof(CWindow) == sizeof(::CWindow), "The `hook` class is not extend the `CWindow` class, and cannot be have additional fields!");
 }
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
-    g_pFetchWindowHook = MAKE_HOOK(handle, fetchTitle, "CWindow");
+    hook::CWindow::fetchTitleHook = make_hook(handle, method_cast(&hook::CWindow::fetchTitle), "CWindow");
+    hook::CWindow::insertWindowToGroupHook =
+        make_hook(handle, method_cast(&hook::CWindow::insertWindowToGroup), "insertWindowToGroupEN9Hyprutils6Memory14CSharedPointerIS_EE", "CWindow");
 
-    if (g_pFetchWindowHook == nullptr) {
-        HyprlandAPI::addNotification(handle, "[group-in-title] Failure in initialization: Failed to find `CWindow::fetchTitle` with compatible signature",
-                                     CColor{1.0, 0.2, 0.2, 1.0}, 7000);
-        throw std::runtime_error("[group-in-title] Hooks fn init failed");
-    }
-
-    if (!g_pFetchWindowHook->hook()) {
-        HyprlandAPI::addNotification(handle, "[group-in-title] Failure in initialization (hook failed)!", CColor{1.0, 0.2, 0.2, 1.0}, 7000);
-        throw std::runtime_error("[group-in-title] Hooks failed");
-    }
+    enable_hooks(hook::CWindow::fetchTitleHook, hook::CWindow::insertWindowToGroupHook);
 
     return {"group-in-title", "A plugin to add group id in window title", "SR_team", "0.1"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
-    if (g_pFetchWindowHook != nullptr)
-        g_pFetchWindowHook->unhook();
+    disable_hooks(hook::CWindow::fetchTitleHook, hook::CWindow::insertWindowToGroupHook);
 }
 
 // Do NOT change this function.
